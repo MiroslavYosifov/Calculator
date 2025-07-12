a46658b0c9d1d044d0d187aa2dca4d6c8f690575
vasil-yordanov14 <vasil_sh@abv.bg>
2025-07-12 23:05:58 +0300

    creating food page filter

---
+++ my-next-app/src/app/lib/foodData.tsx
@@ -0,0 +1,27 @@
+export const foodData = [
+  {
+    id: 1,
+    name: "ябълки",
+    image: "/images/products/apple.jpg",
+  },
+  {
+    id: 2,
+    name: "портокали",
+    image: "/images/products/orange.jpg",
+  },
+  {
+    id: 3,
+    name: "банани",
+    image: "/images/products/banana.jpg",
+  },
+  {
+    id: 4,
+    name: "грозде",
+    image: "/images/products/grape.jpg",
+  },
+  {
+    id: 5,
+    name: "диня",
+    image: "/images/products/watermelon.jpg",
+  },
+];
