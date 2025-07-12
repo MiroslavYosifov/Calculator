379c3010fc5cfd9252923d1e2cbdf8afae26c0b8
unknown <m.yosifov193@gmail.com>
2025-07-12 18:29:09 +0300

    gwdsg

--- my-next-app/src/app/components/Nav.tsx
+++ my-next-app/src/app/components/Nav.tsx
@@ -4,7 +4,9 @@ export default function Nav() {
   return (
     <nav style={{ padding: 10 }}>
       <Link href="/" style={{ marginRight: 10 }}>Home</Link>
-      <Link href="/about">About</Link>
+      <Link href="/about" style={{ marginRight: 10 }}>About</Link>
+      <Link href="/food" style={{ marginRight: 10 }}>Food</Link>
+      <Link href="/workout" style={{ marginRight: 10 }}>Workout</Link>
     </nav>
   );
 }
