53c922ecdcfc522845c11e8d73ffeba39adbee60
unknown <m.yosifov193@gmail.com>
2025-07-26 23:02:14 +0300

    add styles

--- my-next-app/src/app/page.tsx
+++ my-next-app/src/app/page.tsx
@@ -3,8 +3,8 @@ import Calendar from './components/calendar/Calendar';
 
 export default function Home() {
   return (
-    <div className="bg-[var(--color-primary)] grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="bg-[var(--color-primary)] font-[family-name:var(--font-geist-sans)]">
+      <main className="p-4">
         <Calendar />
       </main>
     </div>
